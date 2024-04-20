59f32103b81338ffbb31662f5ab817a1cc425341
Ginnu G <156671422+ginnuganesan@users.noreply.github.com>
2024-04-20 20:06:51 +0530

    Create Medium_152_MaximumProductSubarray.java

---
+++ Medium_152_MaximumProductSubarray.java
@@ -0,0 +1,54 @@
+/* PROBLEM DESCRIPTION
+
+Given an integer array nums, find a 
+subarray
+ that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+*/
+
+//Solution
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int maxCurrProd = 1;
+        int maxTotalProd = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            maxCurrProd *= nums[i];
+            maxTotalProd = Math.max(maxTotalProd, maxCurrProd);
+            if(maxCurrProd == 0) {
+                maxCurrProd = 1;
+            }
+        }
+        maxCurrProd = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            maxCurrProd *= nums[i];
+            maxTotalProd = Math.max(maxTotalProd, maxCurrProd);
+            if(maxCurrProd == 0) {
+                maxCurrProd = 1;
+            }
+        }
+        return maxTotalProd;
+    }
+}
