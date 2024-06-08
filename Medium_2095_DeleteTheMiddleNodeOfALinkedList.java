c29fa8b423c30d33776bfb543fc42b20d84c52e6
Ginnu G <156671422+ginnuganesan@users.noreply.github.com>
2024-06-08 18:36:00 +0530

    Updated another approach without third pointer

--- Medium_2095_DeleteTheMiddleNodeOfALinkedList.java
+++ Medium_2095_DeleteTheMiddleNodeOfALinkedList.java
@@ -71,3 +71,22 @@ class Solution {
         return head;
     }
 }
+
+//Another approach without third pointer
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        fast = fast.next.next; //skip slow traverse one step to stop slow one before mid
+        while(fast != null &&  fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
