308b2717f6f7516ddedcb3f7e6d114c62e1e3051
Ginnu G <156671422+ginnuganesan@users.noreply.github.com>
2024-08-27 19:54:12 +0530

    Create Medium_17. Letter Combinations of a Phone Number.java

---
+++ Medium_17. Letter Combinations of a Phone Number.java
@@ -0,0 +1,53 @@
+/* PROBLEM DESCRIPTION
+
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+ 
+
+Example 1:
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Example 2:
+
+Input: digits = ""
+Output: []
+Example 3:
+
+Input: digits = "2"
+Output: ["a","b","c"]
+ 
+
+Constraints:
+
+0 <= digits.length <= 4
+digits[i] is a digit in the range ['2', '9'].
+
+*/
+
+//Solution
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> out = new ArrayList<>();
+        if(digits.length() == 0) {
+            return out;
+        }
+        String[] map = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        out.add("");
+        for(char digit: digits.toCharArray()) {
+            List<String> list = new ArrayList<>();
+            String word = map[digit - '0'];
+            for(String str: out) {
+                for(char ch: word.toCharArray()) {
+                    list.add(str + ch);
+                }
+            }
+            out = list;
+        }
+        return out;
+    }
+}
