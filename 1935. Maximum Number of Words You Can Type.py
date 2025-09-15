44a6e7369979b9ce75735febcfb67334ffa10a63
Ginnu G <156671422+ginnuganesan@users.noreply.github.com>
2025-09-15 12:04:42 +0530

    Create 1935. Maximum Number of Words You Can Type.py
    Accepted
    20 / 20 testcases passed
    Runtime
    3 ms
    Beats 36.59%
    Memory
    17.95 MB
    Beats 36.81%

---
+++ 1935. Maximum Number of Words You Can Type.py
@@ -0,0 +1,49 @@
+''' PROBLEM DESCRIPTION
+
+There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
+
+Given a string text of words separated by a single space (no leading or trailing spaces) and a string brokenLetters of all distinct letter keys that are broken, return the number of words in text you can fully type using this keyboard.
+
+ 
+
+Example 1:
+
+Input: text = "hello world", brokenLetters = "ad"
+Output: 1
+Explanation: We cannot type "world" because the 'd' key is broken.
+Example 2:
+
+Input: text = "leet code", brokenLetters = "lt"
+Output: 1
+Explanation: We cannot type "leet" because the 'l' and 't' keys are broken.
+Example 3:
+
+Input: text = "leet code", brokenLetters = "e"
+Output: 0
+Explanation: We cannot type either word because the 'e' key is broken.
+ 
+
+Constraints:
+
+1 <= text.length <= 104
+0 <= brokenLetters.length <= 26
+text consists of words separated by a single space without any leading or trailing spaces.
+Each word only consists of lowercase English letters.
+brokenLetters consists of distinct lowercase English letters.
+
+'''
+
+#Solution
+
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        wordsList = text.split()
+        canBeTypedWords = 0
+        for word in wordsList:
+            brLetCount = 0
+            for char in brokenLetters:
+                if char in word:
+                    brLetCount = brLetCount + 1
+            if brLetCount == 0:
+                canBeTypedWords = canBeTypedWords + 1
+        return canBeTypedWords
